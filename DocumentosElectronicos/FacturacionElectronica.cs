7088ece10f8f7b011c841f66625f6e726739999a
Leonardo Chuello <ljchuello@gmail.com>
2019-08-07 16:24:58 -0500

    Cambiando cosas!.

---
+++ DocumentosElectronicos/FacturacionElectronica.cs
@@ -0,0 +1,10 @@
+﻿namespace DocumentosElectronicos
+{
+    public static class FacturacionElectronica
+    {
+        public static void FirmarXml(string origen, string destino, string rutaFirma, string contraseniaFirma)
+        {
+
+        }
+    }
+}
--- DocumentosElectronicos/Program.cs
+++ DocumentosElectronicos/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            string xmlSinFirmar = @"C:\firmas\XmlSinFirmar\2507201901179232604400110010020000000170000001718.xml";
+            string xmlDestino = @"C:\Users\LJChuello\Documents\Sri\Firmados\2507201901179232604400110010020000000170000001718.xml";
+            string firmaElectronica = @"C:\firmas\sermatick.p12";
+            string contraseniaFirma = "LARA2019";
 
+            FacturacionElectronica.FirmarXml(xmlSinFirmar, xmlDestino, firmaElectronica, contraseniaFirma);
         }
     }
 }
